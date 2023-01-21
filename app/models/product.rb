d56113dd7ba495cd3564cc4367cf2d3841af3a6e
Madeline Vogel <madmvogel@gmail.com>
2023-01-21 15:05:01 -0600

    adds is_discounted?, tax, and total model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,34 @@
 class Product < ApplicationRecord
+  # def is_discounted?
+  #   if price <= 10
+  #   return true
+  # end
+
+  # while true
+  #   puts "Enter a number: "
+  #   input = gets.chomp
+  #   if input.to_i > 10
+  #     break
+  #   end
+  # end
+
+  def is_discounted?
+    if price <= 10
+      result = true
+      return result
+    else
+      result = false
+      return result
+    end
+  end
+
+  def tax
+    result = price * 0.09
+    return result
+  end
+
+  def total
+    result = price + tax
+    return result
+  end
 end
