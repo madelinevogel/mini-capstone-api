4803e6a1bb4b25d8345817da3e30c5b1f63fac62
Madeline Vogel <madmvogel@gmail.com>
2023-01-23 13:48:47 -0600

    updated tests due to changes in migrations columns

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -30,7 +30,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     data = JSON.parse(response.body)
     assert_equal "Updated name", data["name"]
-    assert_equal product.price, data["price"]
+    assert_equal product.price.to_s, data["price"]
     assert_equal product.image_url, data["image_url"]
     assert_equal product.description, data["description"]
   end
