bff1b1c6bc791d45babf21af11f1640361ec3280
Madeline Vogel <madmvogel@gmail.com>
2023-01-27 12:50:06 -0600

    adds user and password

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,12 @@ supplier3 = Supplier.create(name: "Lululemon", email: "lulu@test.com", phone_num
 
 product = Product.create(name: "Trampoline", price: 300, description: "10ft x 6ft rectangle outdoor trampoline", inventory: 6, supplier_id: supplier1.id)
 Image.create(url: "https://secure.img1-cg.wfcdn.com/im/16324586/resize-h445%5Ecompr-r85/2264/226451668/14%27+Round+Trampoline+with+Safety+Enclosure.jpg", product_id: product.id)
+Image.create(url: "https://cdn.shopify.com/s/files/1/0003/7715/9724/products/jumpflex_trampoline_spring_pad_safety_mat_d5d22863-57c2-487b-bd4e-f3b130649ce3_2000x.jpg?v=1661209118", product_id: product.id)
 
 product = Product.create(name: "Tea Kettle", price: 34, description: "red tea kettle to heat water", inventory: 6, supplier_id: supplier1.id)
-Image.create(url: "", product_id: product.id)
+Image.create(url: "https://cdn.shopify.com/s/files/1/2643/3792/products/Fellow_stovetop-kettle-steeped-coffee-02-nb_9e3b47db-3f8a-439f-aa84-8ce97dce7b1e_5000x.jpg?v=1515025354", product_id: product.id)
+Image.create(url: "https://m.media-amazon.com/images/I/615rwte88UL.jpg", product_id: product.id)
 
 product = Product.create(name: "Blanket", price: 40, description: "fuzzy white blanket", inventory: 6, supplier_id: supplier3.id)
-Image.create(url: "", product_id: product.id)
+Image.create(url: "https://cdni.llbean.net/is/image/wim/513008_44281_41?hei=1092&wid=950&resMode=sharp2&defaultImage=llbprod/A0211793_2", product_id: product.id)
+Image.create(url: "https://cdni.llbean.net/is/image/wim/513008_0_45?hei=1092&wid=950&resMode=sharp2&defaultImage=llbprod/A0211793_2", product_id: product.id)
