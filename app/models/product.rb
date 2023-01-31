90aee3e8615102d347d9776ed1674645c456b38a
Madeline Vogel <madmvogel@gmail.com>
2023-01-31 13:09:20 -0600

    adds category model and category_product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :supplier
   has_many :orders
+  has_many :category_products
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :price, presence: true
